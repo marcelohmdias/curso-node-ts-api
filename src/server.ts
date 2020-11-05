7b6d69dfb77b333dd2c5b365af71db9819790c78
Marcelo H M Dias <marcelohmdias@gmail.com>
2020-11-05 20:35:52 -0300

    feat: adding server core

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './util/module-alias'
+
+import { Server } from '@overnightjs/core'
+import bodyParser from 'body-parser'
+import { Application } from 'express'
+
+import { ForecastController } from './controller/forecast'
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super()
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json())
+    this.setupControllers()
+  }
+
+  private setupControllers(): void {
+    this.addControllers(new ForecastController())
+  }
+
+  public async init(): Promise<void> {
+    this.setupExpress()
+    this.setupControllers()
+  }
+
+  public getApp(): Application {
+    return this.app
+  }
+}
